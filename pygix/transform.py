0fe7d474ebd3da052f60c504869b90f6d46bd743
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-03-18 11:15:47 +0100

    updated structure, includes doc.

--- pygix/transform.py
+++ pygix/transform.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# REQUIRES pyFAI v 0.10 or greater 
+#
+#    Copyright (C) European Synchrotron Radiation Facility, Grenoble, France
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import absolute_import, print_function, division
 
-__author__ = "Thomas Dane, Jerome Kieffer"
+__authors__ = ["Thomas Dane", "Jérôme Kieffer"]
+__license__ = "GPLv3+"
+__date__ = "18/03/2016"
 __contact__ = "dane@esrf.fr"
 __license__ = "GPLv3+"
 __copyright__ = "ESRF - The European Synchrotron, Grenoble, France"
-__date__ = "18/11/2014"
 __status__ = "Development"
 __docformat__ = "restructuredtext"
 
